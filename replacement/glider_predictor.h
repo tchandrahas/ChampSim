c16746996c00b530a22c2d8348a8a561823de226
Chandrahas Tirumalasetty <chandrahas996@gmail.com>
2020-12-05 14:04:54 -0600

    Added glider cache replacement files, but need checking functionality

---
+++ replacement/glider_predictor.h
@@ -0,0 +1,328 @@
+using namespace std;
+#include <iostream>
+#include <math.h>
+#include <set>
+#include <vector>
+#include <map>
+#include <array>
+#define NUM_PC_PCHR 5
+#define NUM_WEIGHTS 16
+#define NUM_ISVM 32
+#define DEBUG_PRINT
+// Each entry in PCHR is modelled by the following struct
+typedef struct pchr_entry{
+  uint64_t PC;
+  unsigned int LRU_count;
+  bool valid;
+}pchr_entry;
+// Modelling PCHR as 5 member(PC) cache by the following class
+class pchr{
+  private:
+  array <pchr_entry,NUM_PC_PCHR> pc_cache;
+  public:
+  // function to probe PCHR, returns true if the checking_PC is in PCHR, else it will return false
+  bool probe(uint64_t checking_PC){
+    // if the checking_PC is present return true
+    for(int i=0;i<NUM_PC_PCHR;i++){
+      if(pc_cache.at(i).PC == checking_PC && pc_cache.at(i).valid == true){
+        #ifdef DEBUG_PRINT
+        cout << "Probe for PC: " << hex << checking_PC << " is successfull at postion: " << i << endl;
+        #endif
+        return true;
+      }
+    }
+    // else return false
+    return false;
+  }
+  // function called if the PC is indeed present in PCHR, just updates the LRU counter, to complete the access
+  bool complete_access(uint64_t access_PC){
+    bool return_value = false;
+    // Access the access_PC, which means that we have to update the LRU counter here
+    for(int i=0;i<NUM_PC_PCHR;i++){
+      if(pc_cache.at(i).PC == access_PC && pc_cache.at(i).valid == true){
+        // Should make the LRU value 4, because we accessed it 
+        pc_cache.at(i).LRU_count = NUM_PC_PCHR-1;
+        return_value = true;
+
+      }
+      else if(pc_cache.at(i).valid == true) {
+        // decrement the LRU value, with a floor of zero
+        pc_cache.at(i).LRU_count = (pc_cache.at(i).LRU_count>0)?(pc_cache.at(i).LRU_count-1):0;
+      }
+    }
+    #ifdef DEBUG_PRINT
+    for(int i=0;i<NUM_PC_PCHR;i++){
+      cout << "LRU count at position "<< i << " is " <<  pc_cache.at(i).LRU_count << endl;
+    }
+    #endif
+    return return_value;
+  }
+  bool insert(uint64_t inserting_PC){
+    bool return_value = false;
+    int insert_position = -1;
+    // find a insert position in pchr, the position can be invalid or with LRU_count zero
+    for(unsigned int i=0;i<NUM_PC_PCHR;i++){
+      if(pc_cache.at(i).valid == false){
+        // found a invalid element, hence we can exit from here
+        insert_position = i;
+        #ifdef DEBUG_PRINT
+        cout << "Inserting the PC: " << hex << inserting_PC << " at the position: " << i << endl;
+        #endif
+        break;
+      }
+      else if(pc_cache.at(i).LRU_count == 0){
+        insert_position = i;
+        #ifdef DEBUG_PRINT
+        cout << "Inserting the PC: " << hex << inserting_PC << " at the position: " << i << endl;
+        #endif
+        break;
+      }
+    }
+    // By the point we are here, we should have an insert position, otherwise something is messed up
+    assert((insert_position >= 0) && (insert_position < NUM_PC_PCHR));
+    // Do the insertion here along with updating the LRU counts of remaining members
+    for (int i=0;i<5;i++){
+      if(i==insert_position){
+        pc_cache.at(i).PC = inserting_PC;
+        pc_cache.at(i).valid = true;
+        pc_cache.at(i).LRU_count = NUM_PC_PCHR-1;
+        return_value = true;
+      }
+      else if(pc_cache.at(i).valid== true){
+        // decrement the LRU value, with a floor of zero
+        pc_cache.at(i).LRU_count = (pc_cache.at(i).LRU_count>0)?(pc_cache.at(i).LRU_count-1):0;
+      }
+    }
+    #ifdef DEBUG_PRINT
+    for(int i=0;i<NUM_PC_PCHR;i++){
+      cout << "LRU count at position "<< i << " is " <<  pc_cache.at(i).LRU_count << endl;
+    }
+    #endif
+    return return_value;
+  }
+  uint64_t access_PC_at(int index){
+    if(pc_cache.at(index).valid = true){
+      return pc_cache.at(index).PC;
+    }
+    else{
+      return 0;
+    }
+  }
+  pchr(void){
+    // initialize the pchr here as this is the consturctor
+    for(int i=0;i<NUM_PC_PCHR;i++){
+      pc_cache.at(i).PC = 0;
+      pc_cache.at(i).LRU_count = 0;
+      pc_cache .at(i).valid = false;
+    }
+  }
+};
+typedef struct isvm_entry{
+  map<unsigned int,unsigned int> weights;
+}isvm_entry;
+class glider_predictor{
+  private:
+  // declare a map that implements the ISVM table, map of a char key and isvm_entry(which is a map of 16 counters)
+  map <unsigned int,isvm_entry> isvm_table;
+  pchr* PCHR;
+  // The paper uses a threshold of 60, this can be changed later based on result, or can be dynamically choosen
+  unsigned int weights_sum_threshold = 60;
+  // The paper uses a traning threshold, to stop incrementing the weights if their sum is greater than certai value
+  unsigned int weights_training_threshold = 100;
+  // hash function implementation
+  unsigned int pchr_entry_hash(uint64_t PCHR_PC){
+  // shift right by whatever the instruction word is and use the 4 bits preceeding that as a hash
+  return (unsigned int)((PCHR_PC >> 2)&(NUM_WEIGHTS-1));
+  }
+  unsigned int ISVM_table_hash(uint64_t access_PC){
+  // shift right by whatever the stride of instruction word is and use the 5 bits preceeding that as a hash
+  return (unsigned int)((access_PC >> 2)&(NUM_ISVM-1));
+  }
+  public:
+  glider_predictor(void){
+    // First initialize the table of table which is isvm tables, whose each is a 16 weights
+    isvm_entry empty_entry;
+    for(int j=0;j<NUM_WEIGHTS;j++){
+      empty_entry.weights.insert(pair<unsigned int,unsigned int>(j,0));
+    }
+    for(int i=0;i<NUM_ISVM;i++){
+      isvm_table.insert(pair<unsigned int,isvm_entry>(i,empty_entry));
+    }
+    // Construct the PCHR object here
+    PCHR = new pchr;
+  }
+  ~glider_predictor(void){
+    // delete the PCHR object thats being used for glider
+    delete PCHR;
+  }
+  bool get_prediction(uint64_t access_PC){
+    // first get the hash of the access_PC to index into ISVM table
+    unsigned int isvm_table_hash = ISVM_table_hash(access_PC);
+    #ifdef DEBUG_PRINT
+      cout << "The hash for accessing PC into ISVM table is  " << isvm_table_hash << endl;
+    #endif
+    // get the iterator by hashing into the map function
+    map<unsigned int,isvm_entry>::iterator isvm_iterator = isvm_table.find(isvm_table_hash);
+    // This iterator shouldnt be the end iterator, asserting it here
+    assert(isvm_iterator!=isvm_table.end());
+    // Get the isvm from the iterator 
+    isvm_entry access_PC_isvm = isvm_iterator->second;
+    // Now access the 5 PC's in PCHR and generate the hashes
+    unsigned int PC_in_PCHR;
+    unsigned int weights_sum = 0;
+    unsigned int PCHR_PC_hash = 0;
+    for(int i =0;i<NUM_PC_PCHR;i++){
+      if(PCHR->access_PC_at(i) == 0){
+        // PCHR is not constructed by this point, probably asking the predictor to insert is the best thing to do ...
+        return true;
+      }
+      else{
+        // get the PC from PCHR
+        PC_in_PCHR = PCHR->access_PC_at(i);
+        // use the PC to generate the hash into the isvm
+        PCHR_PC_hash = pchr_entry_hash(PC_in_PCHR);
+        #ifdef DEBUG_PRINT
+        cout << "A PC in PCHR resulted in hash of " << PCHR_PC_hash << endl;
+        #endif
+        // use the hash to get the weight
+        map<unsigned int, unsigned int>::iterator  weight_iterator = access_PC_isvm.weights.find(PCHR_PC_hash);
+        // Make sure the iterator is valid, by checking it against the end of the weight map
+        assert(weight_iterator != access_PC_isvm.weights.end());
+        // Accumulate the weights in weights_sum
+        weights_sum += weight_iterator->second;
+      }
+    }
+    #ifdef DEBUG_PRINT 
+    cout << "The sum of weights obtained by the PC's from PCHR hashing into the ISVM is "<< weights_sum << endl; 
+    #endif
+    // If the program has reached this point this means that we have already collected all the weights from PC's in PCHR
+    if(weights_sum > weights_sum_threshold){
+      return true;
+    }
+    else{ 
+      return false;
+    }
+  }
+  bool positive_train_predictor(uint64_t access_PC){
+    // To train the predictor, first we need to hash in the access PC to get a ISVM
+    unsigned int isvm_table_hash = ISVM_table_hash(access_PC);
+    #ifdef DEBUG_PRINT
+      cout << "The hash for accessing PC into ISVM table is  " << isvm_table_hash << endl;
+    #endif
+    // get the iterator by hashing into the map function
+    map<unsigned int,isvm_entry>::iterator isvm_iterator = isvm_table.find(isvm_table_hash);
+    // This iterator shouldnt be the end iterator, asserting it here
+    assert(isvm_iterator!=isvm_table.end());
+    // Get the isvm from the iterator 
+    isvm_entry access_PC_isvm = isvm_iterator->second;
+    // Now access the 5 PC's in PCHR and generate the hashes
+    unsigned int PC_in_PCHR;
+    unsigned int weights_sum = 0;
+    unsigned int PCHR_PC_hash = 0;
+    // Make sure the PCHR is full before proceeding a head, if its half full no problem just return true
+    for(int i =0;i<NUM_PC_PCHR;i++){
+      if(PCHR->access_PC_at(i) == 0){
+        return true;
+      }
+    }
+    // Then obtain the hash from each PC in PCHR in to isvm to obtain weights
+    for(int i =0;i<NUM_PC_PCHR;i++){
+      PC_in_PCHR = PCHR->access_PC_at(i);
+      // use the PC to generate the hash into the isvm
+      PCHR_PC_hash = pchr_entry_hash(PC_in_PCHR);
+      #ifdef DEBUG_PRINT
+      cout << "A PC in PCHR resulted in hash of " << PCHR_PC_hash << endl;
+      #endif
+      // use the hash to get the weight
+      map<unsigned int, unsigned int>::iterator  weight_iterator = access_PC_isvm.weights.find(PCHR_PC_hash);
+      // Make sure the iterator is valid, by checking it against the end of the weight map
+      assert(weight_iterator != access_PC_isvm.weights.end());
+      // Accumulate the weights in weights_sum
+      weights_sum += weight_iterator->second;
+      #ifdef DEBUG_PRINT
+      cout << "The weight obtained is " << weight_iterator->second << endl;
+      #endif
+    }  
+    // Increment the weights if their threshold doesnt exceed 100
+    if(weights_sum < 100){
+      #ifdef DEBUG_PRINT
+      cout << "Incrementing the weights associated with ISVM of " << hex << access_PC << endl; 
+      #endif
+      for(int i=0;i<NUM_PC_PCHR;i++){
+        // get the PC from PCHR
+        PC_in_PCHR = PCHR->access_PC_at(i);
+        // use the PC to generate the hash into the isvm
+        PCHR_PC_hash = pchr_entry_hash(PC_in_PCHR);
+        #ifdef DEBUG_PRINT
+        cout << "A PC in PCHR resulted in hash of " << PCHR_PC_hash << endl;
+        #endif
+        // use the hash to get the weight
+        map<unsigned int, unsigned int>::iterator  weight_iterator = access_PC_isvm.weights.find(PCHR_PC_hash);
+        // Make sure the iterator is valid, by checking it against the end of the weight map
+        assert(weight_iterator != access_PC_isvm.weights.end());
+        // Increment the each weight by 1
+        weight_iterator->second += 1;
+      }
+    }
+    else{
+      #ifdef DEBUG_PRINT
+      cout << "The sum of weights is more than training threshold, so we have not increment the weights" << endl;
+      #endif
+    }
+    return true; 
+  }
+  bool negative_train_predictor(uint64_t access_PC){
+    //To train the predictor, first we need to hash in the access PC to get a ISVM
+    unsigned int isvm_table_hash = ISVM_table_hash(access_PC);
+    #ifdef DEBUG_PRINT
+      cout << "The hash for accessing PC into ISVM table is  " << isvm_table_hash << endl;
+    #endif
+    // get the iterator by hashing into the map function
+    map<unsigned int,isvm_entry>::iterator isvm_iterator = isvm_table.find(isvm_table_hash);
+    // This iterator shouldnt be the end iterator, asserting it here
+    assert(isvm_iterator!=isvm_table.end());
+    // Get the isvm from the iterator 
+    isvm_entry access_PC_isvm = isvm_iterator->second;
+    // Now access the 5 PC's in PCHR and generate the hashes
+    unsigned int PC_in_PCHR;
+    unsigned int weights_sum = 0;
+    unsigned int PCHR_PC_hash = 0;
+    // Make sure the PCHR is full before proceeding a head, if its half full no problem just return true
+    for(int i =0;i<NUM_PC_PCHR;i++){
+      if(PCHR->access_PC_at(i) == 0){
+        return true;
+      }
+    }
+    // Then obtain the hash from each PC in PCHR in to isvm to obtain weights
+    for(int i =0;i<NUM_PC_PCHR;i++){
+      PC_in_PCHR = PCHR->access_PC_at(i);
+      // use the PC to generate the hash into the isvm
+      PCHR_PC_hash = pchr_entry_hash(PC_in_PCHR);
+      #ifdef DEBUG_PRINT
+      cout << "A PC in PCHR resulted in hash of " << PCHR_PC_hash << endl;
+      #endif
+      // use the hash to get the weight
+      map<unsigned int, unsigned int>::iterator  weight_iterator = access_PC_isvm.weights.find(PCHR_PC_hash);
+      // Make sure the iterator is valid, by checking it against the end of the weight map
+      assert(weight_iterator != access_PC_isvm.weights.end());
+      // Decrement the weights with a basement of 0
+      weight_iterator->second = (weight_iterator->second > 0)?(weight_iterator->second-1):0;
+      #ifdef DEBUG_PRINT
+      cout << "The weight obtained is " << weight_iterator->second << endl;
+      #endif
+    }
+    return true; 
+  }
+  bool complete_access(uint64_t access_PC){
+    // This function is to complete the access of a access PC, after getting the prediction and traning
+    // Probe if the PCHR already contains the access PC
+    if(PCHR->probe(access_PC) == true){
+      assert(complete_access(access_PC));
+    }
+    else{
+      // this means that access_PC is not in PCHR, we have to insert it 
+      assert(PCHR->insert(access_PC));
+    }
+    return true;
+  }
+};
---
+++ replacement/optgen.h
@@ -0,0 +1,269 @@
+// Based on optgen code obtained from Cache Replacement Championship(crc2)
+// Website --> https://crc2.ece.tamu.edu/?page_id=53
+using namespace std;
+// Size of each liveliness vector defined below. This tracks the 8x history of 16 way cache, Hawkeye paper says to quantize the accesses by 4. in which case, this should be lesser (1/4th of 128) ??. Leaving it here like this, for now
+#define OPTGEN_VECTOR_SIZE 128
+// Optgen uses Sampler Cache to track all 8x previous access time stamp, the dimensions of the cache are defined below
+// 2800 number derived from Hawkeye paper, can be changed in future
+// ways also chossen randomly, doesnt matter much i think
+#define SAMPLER_CACHE_SIZE 2800
+#define SAMPLER_WAYS 8
+#define SAMPLER_SETS SAMPLER_CACHE_SIZE/SAMPLER_WAYS
+#define SAMPLER_SET_MASK  511
+#define TIMER_SIZE 1024
+#include <iostream>
+#include <math.h>
+#include <set>
+#include <vector>
+// Do CRC for tag caluclation
+uint64_t CRC( uint64_t _blockAddress){
+    static const unsigned long long crcPolynomial = 3988292384ULL;
+    unsigned long long _returnVal = _blockAddress;
+    for( unsigned int i = 0; i < 32; i++ )
+        _returnVal = ( ( _returnVal & 1 ) == 1 ) ? ( ( _returnVal >> 1 ) ^ crcPolynomial ) : ( _returnVal >> 1 );
+    return _returnVal;
+}
+// information tracked for each address in the sampled set
+// last quanta needed to upate the liveliness interval
+// We need not track the Previous-PC for glider, unlike Hawkeye, we wont be updating weights corresponding to previous PC
+// Instead we will be updating the table values for current PC and PCHR
+struct ADDR_INFO{
+    uint64_t addr;
+    uint32_t last_quanta;
+    uint64_t PC;
+    uint32_t lru;
+    // function to initialize the state of each ADDR_INFO
+    void init(unsigned int curr_quanta){
+        last_quanta = 0;
+        PC = 0;
+        lru = 0;
+    }
+    // function to update the state of ADDR_INFO on access to given addr
+    void update(unsigned int curr_quanta, uint64_t _pc, bool prediction){
+        last_quanta = curr_quanta;
+        PC = _pc;
+    }
+};
+class sampler_cache{
+    private:
+    // sampler cache implemented as vector of maps 
+    vector<map<uint64_t, ADDR_INFO> > addr_history;
+    public:
+    // constructor for sampler_cache which initializes the sampler cache
+    sampler_cache(){
+        //initialize the sampler cache
+        addr_history.resize(SAMPLER_SETS);
+        // Now initialize all entries in map 
+        for(int i=0;i<SAMPLER_WAYS;i++){
+            addr_history[i].clear();
+        }
+    }
+    // destructor for sampler_cache 
+    ~sampler_cache(){
+        // Now clear each maps first
+        for(int i=0;i<SAMPLER_WAYS;i++){
+            addr_history[i].clear();
+        }
+        // Then clean the vector
+        addr_history.clear();
+    }
+    // returns the hash into the sampling cache, which is 
+    uint32_t hash_sample_set(uint64_t addr){
+        return (uint32_t) ((addr >> 6) % SAMPLER_SETS);
+    }
+    // procedure for replacing an element in a given set of sampling set 
+    void replace_addr_history_element(unsigned int sampler_set){
+        // initialize the lru_address to 0
+        uint64_t lru_addr = 0;
+        // iterate through the given map at poistion using iterators
+        for(map<uint64_t, ADDR_INFO>::iterator it=addr_history[sampler_set].begin(); it != addr_history[sampler_set].end(); it++){
+            if((it->second).lru == (SAMPLER_WAYS-1)){
+                //lru_time =  (it->second).last_quanta;
+                lru_addr = it->first;
+                break;
+            }
+        }
+        addr_history[sampler_set].erase(lru_addr);
+    }
+    // procedure for updating addr_history 
+    void update_addr_history_lru(unsigned int sampler_set, unsigned int curr_lru){
+        for(map<uint64_t, ADDR_INFO>::iterator it=addr_history[sampler_set].begin(); it != addr_history[sampler_set].end(); it++){
+            if((it->second).lru < curr_lru){
+                (it->second).lru++;
+                assert((it->second).lru < SAMPLER_WAYS); 
+            }
+        }
+    }
+    // procedure for probing into addr_history, returns true if present, false if its not 
+    bool probe_sampler_cache(uint32_t sampler_set, uint64_t sampler_tag){
+        if((addr_history[sampler_set].find(sampler_tag) != addr_history[sampler_set].end())){
+            return true;
+        }
+        else if(addr_history[sampler_set].find(sampler_tag) == addr_history[sampler_set].end()){
+            return false;
+        }
+    }
+    // Function to add element into SamplerCache
+    bool add_element(uint32_t sampler_set, uint64_t sampler_tag,uint64_t curr_quanta){
+        // First check if the number of ways are full
+        if(addr_history[sampler_set].size() == SAMPLER_WAYS){
+            #ifdef DEBUG_PRINT
+            cout << "sampler_set: " << sampler_set << " was found to be full, will be replacing one" << endl;
+            #endif
+            replace_addr_history_element(sampler_set);
+        }
+        // By this point we should have removed atleast one element from the set
+        assert(addr_history[sampler_set].size() < SAMPLER_WAYS);
+        // Now initialize the block we want, not sure if this works or not, coz there is no mapping entry for sampler_tag
+        addr_history[sampler_set][sampler_tag].init(curr_quanta);
+        // Update the LRU as well, as this is placed just now
+        update_addr_history_lru(sampler_set, SAMPLER_WAYS-1);
+        return true;
+    }
+    // Function to get the last quanta, returns it in the form of uint64_t 
+    uint64_t get_last_quanta(uint32_t sampler_set, uint64_t sampler_tag){
+        assert((addr_history[sampler_set].find(sampler_tag) != addr_history[sampler_set].end()));
+        map<uint64_t, ADDR_INFO>::iterator it = addr_history[sampler_set].find(sampler_tag);
+        return it->second.last_quanta;
+    }
+    // Function to get the last quanta, returns it in the form of uint64_t 
+    uint32_t get_lru(uint32_t sampler_set, uint64_t sampler_tag){
+        assert((addr_history[sampler_set].find(sampler_tag) != addr_history[sampler_set].end()));
+        map<uint64_t, ADDR_INFO>::iterator it = addr_history[sampler_set].find(sampler_tag);
+        return it->second.lru;
+    }
+};
+typedef struct OPTgen_entry{
+    // Tracks the liveness_history per set
+    vector<unsigned int> liveness_history;
+    uint64_t CACHE_SIZE;
+    // Numbers to tracks the decision statistics
+    uint64_t num_cache;
+    uint64_t num_dont_cache;
+    uint64_t access;
+    // function to initialize each OPTgen_entry
+    void init(uint64_t size){
+        num_cache = 0;
+        num_dont_cache = 0;
+        access = 0;
+        CACHE_SIZE = size;
+        liveness_history.resize(OPTGEN_VECTOR_SIZE, 0);
+    }
+
+    void add_access(uint64_t curr_quanta){
+        access++;
+        liveness_history[curr_quanta] = 0;
+    }
+    /*
+    void add_prefetch(uint64_t curr_quanta){
+        liveness_history[curr_quanta] = 0;
+    }
+    */
+    bool should_cache(uint64_t curr_quanta, uint64_t last_quanta)
+    {
+        bool is_cache = true;
+        unsigned int i = last_quanta;
+        while (i != curr_quanta){
+            if(liveness_history[i] >= CACHE_SIZE){
+                is_cache = false;
+                break;
+            }
+
+            i = (i+1) % liveness_history.size();
+        }
+
+
+        //if ((is_cache) && (last_quanta != curr_quanta))
+        if ((is_cache)){
+            i = last_quanta;
+            while (i != curr_quanta){
+                liveness_history[i]++;
+                i = (i+1) % liveness_history.size();
+            }
+            assert(i == curr_quanta);
+        }
+
+        if (is_cache) num_cache++;
+        else num_dont_cache++;
+
+        return is_cache;    
+    }
+
+    uint64_t get_num_opt_hits(){
+        return num_cache;
+
+        uint64_t num_opt_misses = access - num_cache;
+        return num_opt_misses;
+    }
+}OPTgen_entry;
+class OPTgen{
+    private:
+    // data structure that has perset OPTgen structures
+    // We use only 16 of these declared static arrays, the hawkeye implementation uses in such a way
+    OPTgen_entry perset_optgen[LLC_SET];
+    uint64_t perset_mytimer[LLC_SET];
+    sampler_cache* SamplerCache;
+    public:
+    // In the constructor, we construct the sampler_cache
+    OPTgen(){
+        SamplerCache = new sampler_cache;
+        for(int i=0;i<LLC_SET;i++){
+            perset_mytimer[i] = 0;
+            perset_optgen[i].init(LLC_WAY-2); // May be this should be LLC_WAYS, not sure why we made it LLC_WAYS-2
+        }
+    }
+    // In the destructor we destrut the sampler_cache 
+    ~OPTgen(){
+        delete SamplerCache;
+    }
+    // This function only checks if the sampling set that has entery in Sampling cache, if it is, it retruns yes, else false
+    bool CheckOPTgen(uint64_t PC,uint64_t paddr,uint32_t set){
+        uint32_t sampler_set = SamplerCache->hash_sample_set(paddr);
+        uint64_t sampler_tag = CRC(paddr >> 12) % 256;
+        #ifdef DEBUG_PRINT
+        cout << "PC: " << hex << PC <<" , PADDR: " << hex << paddr << " , sampler_set: " << sampler_set << ", sampler_tag: " << sampler_tag << endl;
+        #endif
+        return SamplerCache->probe_sampler_cache(sampler_set,sampler_tag);
+    }
+    // This function is only called after knowing that given set has an entry in the Sampling Cache 
+    bool GetOPTgenDecision(uint64_t PC,uint64_t paddr,uint32_t set){
+        //The current timestep 
+        uint64_t curr_quanta = perset_mytimer[set] % OPTGEN_VECTOR_SIZE;
+        unsigned int curr_timer = perset_mytimer[set];
+        uint32_t sampler_set = SamplerCache->hash_sample_set(paddr);
+        uint64_t sampler_tag = CRC(paddr >> 12) % 256;
+        uint64_t last_quanta = SamplerCache->get_last_quanta(sampler_set,sampler_tag);
+        if(curr_timer < last_quanta)
+            curr_timer = curr_timer + TIMER_SIZE;
+        bool wrap =  ((curr_timer -last_quanta) > OPTGEN_VECTOR_SIZE);
+        last_quanta = last_quanta % OPTGEN_VECTOR_SIZE;
+        if( !wrap && perset_optgen[set].should_cache(curr_quanta, last_quanta)){
+            // Positively train the predictor
+            return true;
+        }
+        else{
+            // Negatively train the predictor
+            return false;
+        }
+    }
+    // this function is called when the CheckOptgen returns false, which means this element missed in SamplerCache
+    bool AddOptgen(uint64_t PC,uint64_t paddr,uint32_t set){
+        uint32_t sampler_set = SamplerCache->hash_sample_set(paddr);
+        uint64_t sampler_tag = CRC(paddr >> 12) % 256;
+        //The current timestep 
+        uint64_t curr_quanta = perset_mytimer[set] % OPTGEN_VECTOR_SIZE;
+        assert(SamplerCache->add_element( sampler_set,  sampler_tag, curr_quanta));
+        perset_optgen[set].add_access(curr_quanta);
+        SamplerCache->update_addr_history_lru(sampler_set,SAMPLER_WAYS-1);
+    }
+    // Function that takes care of some book-keeping that neeeds to be done after obtaining the prediction
+    bool CompleteOptgen(uint64_t PC,uint64_t paddr,uint32_t set){
+        int32_t sampler_set = SamplerCache->hash_sample_set(paddr);
+        uint64_t sampler_tag = CRC(paddr >> 12) % 256;
+        //Increment the set timer
+        perset_mytimer[set] = (perset_mytimer[set]+1) % TIMER_SIZE;
+        //addr_history[sampler_set][sampler_tag].update(perset_mytimer[set], PC, new_prediction);
+        //addr_history[sampler_set][sampler_tag].lru = 0;
+    }
+    
+};
